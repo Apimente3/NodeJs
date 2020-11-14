e3179c49323e922b7cccb6acd749bf960e1be7ba
Juan Kaenel <kaenel10@gmail.com>
2020-11-14 15:43:21 -0300

    arreglando config bd

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config({path: 'variables.env'})
 
 // Nombre de la bd, user, password, 
-const db = new Sequelize(process.env.DB_NOMBRE,process.env.DB_USER,process.env.DB_PASSWORD,{
+const db = new Sequelize(process.env.DB_NOMBRE,process.env.DB_USER,process.env.DB_PASS,{
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: 'mysql',
